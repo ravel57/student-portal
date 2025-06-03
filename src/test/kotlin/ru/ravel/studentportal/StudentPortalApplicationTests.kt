c8a4c99d950f3827749d647bcc4dc90c9ee9513e
petr <petyalom@yandex.ru>
2025-06-03 10:04:03 +0300

    first commit

---
+++ src/test/kotlin/ru/ravel/studentportal/StudentPortalApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.ravel.studentportal
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StudentPortalApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
